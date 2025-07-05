8fd883f419c79ea6fb25455f1e084d115213d5f9
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-07-05 23:53:02 +0530

    ui-update..minor

--- lib/Normal_bus_time_IC_update.dart
+++ lib/Normal_bus_time_IC_update.dart
@@ -30,59 +30,64 @@ class _Normal_bustime_ICState extends State<Normal_bustime_IC> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Normal Bus Time Table Update')),
-      body: Column(
-        children: [
-          TextField(
-            controller: busStartLocationController,
-            decoration: InputDecoration(labelText: 'Start Location'),
-          ),
-          TextField(
-            controller: busEndLocationController,
-            decoration: InputDecoration(labelText: 'End Location'),
-          ),
-          TextField(
-            controller: busStartTimeController,
-            decoration: InputDecoration(labelText: 'Start Time'),
-          ),
-          TextField(
-            controller: busNmberController,
-            decoration: InputDecoration(labelText: 'Bus Number'),
-          ),
-          TextField(
-            controller: busdriverNameController,
-            decoration: InputDecoration(labelText: 'Driver Name'),
-          ),
-          TextField(
-            controller: busTPnumberController,
-            decoration: InputDecoration(labelText: 'Driver TP Number'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Map<String, String> normalbustimetableIc = {
-                'Start Location': busStartLocationController.text,
-                'End Location': busEndLocationController.text,
-                'Start Time': busStartTimeController.text,
-                'Bus Number': busNmberController.text,
-                'Driver Name': busdriverNameController.text,
-                'Driver TP Number': busTPnumberController.text,
-              };
-              databaseReference
-                  .push()
-                  .set(normalbustimetableIc)
-                  .then((_) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Data Updated Successfully')),
-                    );
-                  })
-                  .catchError((error) {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Failed to update data: $error')),
-                    );
-                  });
-            },
-            child: Text('Update Bus Time Table'),
-          ),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(13.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: busStartLocationController,
+              decoration: InputDecoration(labelText: 'Start Location'),
+            ),
+            TextField(
+              controller: busEndLocationController,
+              decoration: InputDecoration(labelText: 'End Location'),
+            ),
+            TextField(
+              controller: busStartTimeController,
+              decoration: InputDecoration(labelText: 'Start Time'),
+            ),
+            TextField(
+              controller: busNmberController,
+              decoration: InputDecoration(labelText: 'Bus Number'),
+            ),
+            TextField(
+              controller: busdriverNameController,
+              decoration: InputDecoration(labelText: 'Driver Name'),
+            ),
+            TextField(
+              controller: busTPnumberController,
+              decoration: InputDecoration(labelText: 'Driver TP Number'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Map<String, String> normalbustimetableIc = {
+                  'Start Location': busStartLocationController.text,
+                  'End Location': busEndLocationController.text,
+                  'Start Time': busStartTimeController.text,
+                  'Bus Number': busNmberController.text,
+                  'Driver Name': busdriverNameController.text,
+                  'Driver TP Number': busTPnumberController.text,
+                };
+                databaseReference
+                    .push()
+                    .set(normalbustimetableIc)
+                    .then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Data Updated Successfully')),
+                      );
+                    })
+                    .catchError((error) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Failed to update data: $error'),
+                        ),
+                      );
+                    });
+              },
+              child: Text('Update Bus Time Table'),
+            ),
+          ],
+        ),
       ),
     );
   }
