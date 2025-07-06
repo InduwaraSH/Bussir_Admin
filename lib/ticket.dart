333d4bd848bcb14663b0e16ef6e3c64de0edaa92
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-07-06 11:49:52 +0530

    connect with db ticket

--- lib/ticket.dart
+++ lib/ticket.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 
 class ticketissue extends StatefulWidget {
@@ -15,8 +16,79 @@ class _ticketissueState extends State<ticketissue> {
   final dateController = TextEditingController();
   final passengerIdController = TextEditingController();
 
+  late DatabaseReference databaseReference_3;
+
+  @override
+  void initState() {
+    super.initState();
+    databaseReference_3 = FirebaseDatabase.instance.ref().child('TicketIssue');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text('Issue Ticket')),
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: busNoController,
+              decoration: InputDecoration(labelText: 'Bus Number'),
+            ),
+
+            TextField(
+              controller: departureTimeController,
+              decoration: InputDecoration(labelText: 'Departure Time'),
+            ),
+            TextField(
+              controller: fromController,
+              decoration: InputDecoration(labelText: 'From'),
+            ),
+            TextField(
+              controller: toController,
+              decoration: InputDecoration(labelText: 'To'),
+            ),
+            TextField(
+              controller: dateController,
+              decoration: InputDecoration(labelText: 'Date'),
+            ),
+            TextField(
+              controller: passengerIdController,
+              decoration: InputDecoration(labelText: 'Passenger ID'),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Map<String, String> ticketdata = {
+                  'Bus Number': busNoController.text,
+                  'Departure Time': departureTimeController.text,
+                  'From': fromController.text,
+                  'To': toController.text,
+                  'Date': dateController.text,
+                  'Passenger ID': passengerIdController.text,
+                };
+                databaseReference_3
+                    .push()
+                    .set(ticketdata)
+                    .then((_) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Data Updated Successfully')),
+                      );
+                    })
+                    .catchError((error) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('Failed to update data: $error'),
+                        ),
+                      );
+                    });
+              },
+              child: Text('Issue Ticket'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
